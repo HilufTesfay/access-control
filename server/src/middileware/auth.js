e7d25a2cbfd0ca3a2832b020f12eac65a1a0be68
Hiluf Tesfay <hiluftesfay43@gmail.com>
2025-01-01 03:20:58 +0600

    add auth.js middleware to autherize and authenticate users

---
+++ server/src/middileware/auth.js
@@ -0,0 +1,28 @@
+import { tokenService } from "../services/index.js";
+import CustomError from "../utils/customError.js";
+import { roleRight } from "../config/roles.js";
+import { userService } from "../services/index.js";
+//authenticate user
+const isAuthenticated = async (req, res, next) => {
+  const token = tokenService.extractToken(req.headers);
+  const { isValidToken, userRole, userId } =
+    tokenService.isAuthenticatedToken(token);
+  if (!isValidToken) {
+    return next(new CustomError(403, "you are not authenticated", "user"));
+  }
+  req.user = await userService.getUserById(userId);
+  req.user.isAuthenticated = true;
+  return next();
+};
+
+//autherize user
+
+const autherizeUser =
+  (...requiredRights) =>
+  (req, res, next) => {
+    if (!req.user || !req.user.isAuthenticated) {
+      return next(new CustomError(403, "you are not authenticated", "user"));
+    }
+    const userRole = req.user.role;
+    const userRights = roleRight.get(userRole);
+  };
