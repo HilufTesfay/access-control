a8345a2e08be3717cc9b55546fe484fc9ed61dcb
Hiluf Tesfay <hiluftesfay43@gmail.com>
2025-01-01 03:23:21 +0600

    add feature to role.js and call errorHandlers in app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,20 @@
 import express from "express";
 import morgan from "morgan";
 import { morganFormat, stream } from "./config/morgan.js";
+import { convertError, handleGlobalError } from "./middileware/errorHandler.js";
+import CustomError from "./utils/customError.js";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan(morganFormat, { stream }));
+
+app.use("*", (req, res, next) => {
+  const message = `this ${req.originalUrl} is not found`;
+  next(new CustomError(404, message, "user"));
+});
+
+//error handler middleware
+app.use(convertError);
+app.use(handleGlobalError);
+
 export default app;
--- server/src/config/roles.js
+++ server/src/config/roles.js
@@ -1,7 +1,12 @@
 const allRoles = {
-  instructor: [""],
-  admin: [""],
-  student: [""],
+  instructor: [
+    "createAssesmet",
+    "deleteAssesment",
+    "updateAssesment",
+    "updateProfile",
+  ],
+  student: ["updateProfile", "viewMarks", "viewAssesments"],
+  admin: ["manageCourses", "manageUsers"],
 };
 const roles = Object.keys(allRoles);
 const roleRight = new Map(Object.entries(allRoles));
